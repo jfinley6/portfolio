306d8c51f98c981e73af4f7be0285f980bbc4600
johnfinley <johnfinleybremerton@gmail.com>
2023-04-14 17:08:53 -0600

    define AppLayout and wrap in createBrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import App from "./App";
+import NavBar from "./components/NavBar";
+import Projects from "./components/Projects";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const AppLayout = () => {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "/projects",
+        element: <Projects />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <div className="app-container">
+      <RouterProvider router={router} />
+    </div>
+  </React.StrictMode>
+);
