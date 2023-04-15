af735c65e2bfbc381b390cb6951a6e1ca0049fa3
johnfinley <johnfinleybremerton@gmail.com>
2023-04-15 13:02:10 -0600

    change title of page with useEffect

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
 function Projects() {
-
   useEffect(() => {
-    document.title = "John Finley | Projects"
-  },[])
-  
-  return (
-    <div>Projects</div>
-  )
+    document.title = "John Finley | Projects";
+  }, []);
+
+  return <div>Projects</div>;
 }
 
-export default Projects
+export default Projects;
