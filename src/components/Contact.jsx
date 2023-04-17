6829194b049aab86bc567e87e5dc71bc49513687
johnfinley <johnfinleybremerton@gmail.com>
2023-04-17 16:45:25 -0600

    add email feature

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,25 +1,69 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useEffect, useState } from "react";
+import emailjs from "@emailjs/browser";
 import "../stylesheets/contact.css";
 
 export default function Contact() {
-  const [formData, setFormData] = useState({name: "", email: "", message: ""})
+  const initalState = {
+    name: "",
+    email: "",
+    message: "",
+  };
+  const [formData, setFormData] = useState(initalState);
+  const [emailStatus, setEmailStatus] = useState("")
 
   useEffect(() => {
     document.title = "John Finley | Contact";
-    window.scrollTo(0,0)
+    window.scrollTo(0, 0);
   }, []);
 
   const handleChange = (event) => {
-    setFormData({...formData, [event.target.name]: event.target.value})
-  }
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const clearState = () => {
+    setFormData({ ...initalState });
+  };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .send(
+        import.meta.env.VITE_SERVICE_ID,
+        import.meta.env.VITE_TEMPLATE_ID,
+        {
+          from_name: formData.name,
+          from_email: formData.email,
+          message: formData.message,
+        },
+        import.meta.env.VITE_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          setEmailStatus("Message Sent Successfully");
+        },
+        (error) => {
+          document.getElementById("email-status").style.color = "red"
+          setEmailStatus(error.text)
+        }
+      )
+      .then(() => {
+        clearState();
+      });
+  };
 
   return (
     <div id="contact-page">
       <div id="contact-info">
         <p>Contact</p>
         <h1>Looking for a new developer or just want to chat?</h1>
-        <p style={{ color: "rgb(145, 148, 151)", fontSize: "large", lineHeight: 1.2 }}>
+        <p
+          style={{
+            color: "rgb(145, 148, 151)",
+            fontSize: "large",
+            lineHeight: 1.2,
+          }}
+        >
           Send me a message to get the process going
         </p>
         <a href="mailto:j.finley92@gmail.com" id="mail-to">
@@ -39,27 +83,54 @@ export default function Contact() {
           j.finley92@gmail.com
         </a>
       </div>
-      <form id="contact-form" autoComplete="off" autoCorrect="off">
+      <form
+        id="contact-form"
+        autoComplete="off"
+        autoCorrect="off"
+        onSubmit={sendEmail}
+      >
         <div className="contact-inputs">
           <div id="name-div">
             <label>
-              NAME <span style={{color: "red"}}>*</span>
+              NAME <span style={{ color: "red" }}>*</span>
             </label>
-            <input type="text" required  name="name" id="name-input" onChange={handleChange}></input>
+            <input
+              type="text"
+              required
+              name="name"
+              id="name-input"
+              onChange={handleChange}
+              value={formData.name}
+            ></input>
           </div>
           <div id="email-div">
             <label>
-              Email <span style={{color: "red"}}>*</span>
+              Email <span style={{ color: "red" }}>*</span>
             </label>
-            <input type="email" required  name="email" id="email-input" onChange={handleChange}></input>
+            <input
+              type="email"
+              required
+              name="email"
+              id="email-input"
+              onChange={handleChange}
+              value={formData.email}
+            ></input>
           </div>
           <div id="message-div">
             <label>
-              Message <span style={{color: "red"}}>*</span>
+              Message <span style={{ color: "red" }}>*</span>
             </label>
-            <textarea type="text" required  name="message" id="message-input" onChange={handleChange}></textarea>
+            <textarea
+              type="text"
+              required
+              name="message"
+              id="message-input"
+              onChange={handleChange}
+              value={formData.message}
+            ></textarea>
           </div>
         </div>
+        <p id="message-status">{emailStatus}</p>
         <button id="email-button">Send</button>
       </form>
     </div>
