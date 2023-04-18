4db15276c2ffab26ccd4dc371747eb7ea0feb4f5
johnfinley <johnfinleybremerton@gmail.com>
2023-04-18 16:49:22 -0600

    add about-intro section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { useEffect } from "react";
+import "../stylesheets/about.css";
 
 function About() {
   useEffect(() => {
     document.title = "John Finley | About";
   }, []);
-  return <div>About</div>;
+  return (
+    <div id="about-page">
+      <div id="about-intro">
+        <p>Who is John Finley?</p>
+        <p>A curious Full Stack Software Engineer who is passionate about learning and is constantly trying to further himself.</p>
+      </div>
+    </div>
+  );
 }
 
 export default About;
