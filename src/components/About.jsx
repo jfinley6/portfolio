cbcea8c0b0c3256c58ed11c48cd5514673fad3a2
johnfinley <johnfinleybremerton@gmail.com>
2023-04-18 18:57:26 -0600

    add about photo and paragraphs

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,19 @@ function About() {
     <div id="about-page">
       <div id="about-intro">
         <p>Who is John Finley?</p>
-        <p>A curious Full Stack Software Engineer who is passionate about learning and is constantly trying to further himself.</p>
+        <p>
+          A curious Full Stack Software Engineer who is passionate about
+          learning and is constantly trying to further himself.
+        </p>
+      </div>
+      <div id="about-content">
+        <img id="about-picture" src="./about_photo.jpg" />
+        <p className="about-paragraphs">
+          Hi! I'm John, a Full Stack Software Engineer currently residing in
+          Denver, CO. I recently graduated from Flatiron School's Live Software
+          Engineering Program and am now looking for my first role in this
+          exciting field.
+        </p>
       </div>
     </div>
   );
