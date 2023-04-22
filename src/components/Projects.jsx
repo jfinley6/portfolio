9e786c02cf701c2a68f40cb02a387032bc4db5e3
johnfinley <johnfinleybremerton@gmail.com>
2023-04-21 20:54:10 -0600

    added a placeholder projects page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,16 @@
 import React, { useEffect } from "react";
+import "../stylesheets/projects.css";
 
 function Projects() {
   useEffect(() => {
     document.title = "John Finley | Projects";
   }, []);
 
-  return <div>Projects</div>;
+  return (
+    <div id="projects-page">
+      <h3>Projects Coming Soon!</h3>
+    </div>
+  );
 }
 
 export default Projects;
