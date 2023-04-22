71e90d749350de5bff2151de906d43dcdea29ec6
johnfinley <johnfinleybremerton@gmail.com>
2023-04-22 09:02:08 -0600

    updated projects page placeholder

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { NavLink, Link } from "react-router-dom";
 import "../stylesheets/projects.css";
 
 function Projects() {
@@ -8,7 +9,13 @@ function Projects() {
 
   return (
     <div id="projects-page">
-      <h3>Projects Coming Soon!</h3>
+      <h3>Project Page Coming Really Soon!</h3>
+      <h4>
+        My current projects can be found on my{" "}
+        <NavLink style={{color: "var(--green)"}} to="/resume">Resume</NavLink>,{" "} 
+        <Link style={{color: "var(--green)"}} to="https://linkedin.com/in/john-tyler-finley">Linkedin</Link>, or{" "}
+        <Link style={{color: "var(--green)"}} to="https://github.com/jfinley6">Github</Link>
+      </h4>
     </div>
   );
 }
