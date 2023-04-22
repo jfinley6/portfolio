a64dbfc44788b1ddaa9bbdaee37547beb8308552
johnfinley <johnfinleybremerton@gmail.com>
2023-04-21 18:58:41 -0600

    resume is now displayed the full width of the page

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import "../stylesheets/resume.css";
 
 function Resume() {
-  return <div id="resume-page"></div>;
+  return (
+    <div id="resume-page">
+      <iframe
+        src="./JohnFinleyResume.pdf#toolbar=0&navpanes=0"
+        height="100%"
+        width="100%"
+      />
+    </div>
+  );
 }
 
 export default Resume;
